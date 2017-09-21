ff178ad99bb09a638e539685ba235c9876ebd203
Nagen robo-ND VM <rao.jogi@gmail.com>
2017-09-20 21:00:08 -0700

    Added Extracting Plane filter, Extracting Indices and Outlier
    Extracting Indices: with the code snippet, the extract point cloud by providing a list of indices
    Outlier Removal: used to remove and take care of distortion, noise in the environment

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -18,8 +18,8 @@ passthrough = cloud_filtered.make_passthrough_filter()
 #Assign axis and range to the passthrough filter object
 filter_axis = 'z'
 passthrough.set_filter_field_name(filter_axis)
-axis_min = 0
-axis_max = 2
+axis_min = 0.6
+axis_max = 1.1
 
 passthrough.set_filter_limits(axis_min, axis_max)
 
@@ -29,12 +29,37 @@ filename = 'pass_through_filtered.pcd'
 
 
 # RANSAC plane segmentation
+ # create the segmentation object
+seg = cloud_filtered.make_segmenter()
 
+ #Set the model
+seg.set_model_type(pcl.SACMODEL_PLANE)
+seg.set_model_type(pcl.SAC_RANSAC)
+
+max_distance = 0.01
+seg.set_distance_threshold(max_distance)
+
+inliers, coefficients = seg.segment()
+extract_inliers = cloud_filtered.extract(inliers, negative=False)
+filename = 'extracted_inliers.pcd'
+
+extract_outliers = cloud_filtered.extract(inliers, negative=True)
+filename = 'extracted_outliers.pcd'
+
+
+outlier_filter = cloud_filtered.make_statistical_outlier_filter()
+outlier_filter.set_mean_k(50)
+
+x = 1.0
+outlier_filter.set_std_dev_mul_thresh(x)
+cloud_filtered = outlier_filter.filter()
+
+pcl.save(extract_outliers, filename)
 
 # Extract inliers
 
 # Save pcd for table
-pcl.save(cloud_filtered, filename)
+#pcl.save(extract_inliers, filename)
 
 # Extract outliers
 
