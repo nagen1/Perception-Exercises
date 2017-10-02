293e75a7e93903f4adb853adfea6f50db80f5362
Nagen robo-ND VM <rao.jogi@gmail.com>
2017-10-02 00:33:42 -0400

    Updates for Object recognition exercise3

--- Exercise-3/sensor_stick/scripts/object_recognition.py
+++ Exercise-3/sensor_stick/scripts/object_recognition.py
@@ -82,20 +82,45 @@ def pcl_callback(pcl_msg):
     # Exercise-3 TODOs:
 
     # Classify the clusters! (loop through each detected cluster one at a time)
+    detected_objects_labels = []
+    detected_objects = []
+
+    for index, pts_list in enumerate(cluster_indices):
 
         # Grab the points for the cluster
-
+        pcl_cluster = ros_cloud_objects.extract(pts_list)
+        ros_cluster = pcl_to_ros(pcl_cluster)
 
         # Compute the associated feature vector
+        # Extract histogram features
+        chists = compute_color_histograms(sample_cloud, using_hsv=True)
+        normals = get_normals(sample_cloud)
+        nhists = compute_normal_histograms(normals)
+        feature = np.concatenate((chists, nhists))
+        labeled_features.append([feature, model_name])
+
 
         # Make the prediction
+        prediction = clf.predict(scaler.transform(feature.reshape(1, -1)))
+        label = encoder.inverse_transform(prediction)[0]
+        detected_objects_labels.append(label)
+
 
         # Publish a label into RViz
+        label_pos = list(white_cloud[pts_list[0]])
+        label_pos[2] += .4
+        object_markers_pub.publish(make_label(label, label_pos, index))
+
 
         # Add the detected object to the list of detected objects.
+        do = DetectObject()
+        do.label = label
+        do.cloud = ros_cluster
+        detected_objects.append(do)
 
+    rospy.loginfo('Detected {} object: {}', format(len(detected_objects_labels), detected_objects_labels))
     # Publish the list of detected objects
-
+    detected_objects_pub.publish(detected_objects)    
 
 if __name__ == '__main__':
 
